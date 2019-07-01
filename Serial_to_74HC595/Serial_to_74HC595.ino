ef8027743b493a02cb853f4c0ffb62d48d28337a
David Bailey <46795205+twsdbailey@users.noreply.github.com>
2019-06-30 22:17:06 -0500

    Updated version

--- Serial_to_74HC595/Serial_to_74HC595.ino
+++ Serial_to_74HC595/Serial_to_74HC595.ino
@@ -1,36 +1,74 @@
 //Serial to 74HC595 shift registers
 //D.Bailey 6/26/2019
 
-//Test this using Serial Monitor or another terminal program
+//Test this using Serial Monitor or another terminal program.
+//See README for more info on how to send the data.
 
-int myArray[16][2]; //first initializer = number of rows,
-                    //second initializer = number of shift registers
+
+//*********configuration*******************
+int dataArray[16][2]; //first initializer = total rows to render, second initializer = total number of shift registers
+int rows = 16; //should match first intitalizer in myArray
+int registers = 2; //should match 2nd intitializer in myArray, each register has 8 outputs
+
+//set timing intervals
+int onTime = 75;  //"on" time of the outputs
+int offTime = 25; //"off" time of the outputs
+
+//define Arduino pinouts to connect to the shift registers
 #define latchPin 8
 #define clockPin 12
 #define dataPin 11
+
+bool debug = true; //set this true to echo input data for troubleshooting back to Serial Monitor or other
+                  //terminal program
+
+
+//********no changes needed below this point*******
+
+//other variables
 int i = 0;
 int j = 0;
 int data = 0;
-byte clearline = B00000000; //byte used to clear shift register
 bool looping = false;
+bool rendered = false;
 String readString;
 
-//set timing intervals here
-int onTime = 75;
-int offTime = 25;
+
+void clearRow() { //this shifts 0s through the registers to turn all outputs off
+  digitalWrite(latchPin, LOW);
+  for (int z = 0; z < 2; z++) { //change to reflect the number of shift registers as needed
+    shiftOut(dataPin, clockPin, MSBFIRST, 0);
+  }
+  digitalWrite(latchPin, HIGH);
+}
+
+
+void renderRow() { //this reads the 2-dimensional array, and turns on the shift register outputs row by row
+                    //number of rows set in the array intializer
+  for (int y = 0; y < rows; y++) { //"y" is the number of rows to render
+    digitalWrite(latchPin, LOW);
+    for (int x = 0; x < registers; x++) { //"x" is the number of registers, eight outputs each
+      shiftOut(dataPin, clockPin, MSBFIRST, dataArray[y][x]);
+    }
+    digitalWrite(latchPin, HIGH);
+    delay(onTime); //on time
+    clearRow();
+    delay (offTime); //off time
+  }
+  clearRow();
+  delay (offTime); //off time
+  i = 0;
+  data = 0;
+}
+
 
 void setup() {
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
-
-  //clear shift registers
-  digitalWrite(latchPin, LOW);
-  for (int z = 0; z < 2; z++) { //change to reflect the number of shift registers as needed
-    shiftOut(dataPin, clockPin, MSBFIRST, clearline);
-  }
-  digitalWrite(latchPin, HIGH);
+  clearRow();//clear shift registers
   Serial.begin(9600);
+
 }
 
 void loop() {
@@ -39,19 +77,19 @@ void loop() {
     if (c == ',') {
       if (readString.length() > 0) {
         data = readString.toInt();  //convert readString into an integer
-        myArray[i][j] = data;
-
-  // debugging
-        Serial.print("data in " );
-        Serial.println(data);
-        Serial.print("line #");
-        Serial.println(i);
-        Serial.print("element #");
-        Serial.println(j);
-        Serial.println("");
-        Serial.println("");
-        
-        j++;
+        dataArray[i][j] = data;
+        if (debug) {
+          // debugging
+          Serial.print("data in " );
+          Serial.println(data);
+          Serial.print("line #");
+          Serial.println(i);
+          Serial.print("element #");
+          Serial.println(j);
+          Serial.println("");
+          Serial.println("");
+        }
+        j++;//increment next byte of data to send to shift registers
         if (j > 1) {
           i++; //increment to next line of data
           j = 0;
@@ -59,57 +97,28 @@ void loop() {
         readString = ""; //clears variable for new input
       }
     }
-    else if (c == 'l')looping = true; //listen for "loop" command
-    else if (c == 's'){
+    else if (c == 'l') {
+      looping = true; //listen for "loop" command
+    }
+    else if (c == 's') {
       looping = false; //listen for "stop" command
-       digitalWrite(latchPin, LOW);
-      for (int z = 0; z < 2; z++) { //change to reflect the number of shift registers as needed
-        shiftOut(dataPin, clockPin, MSBFIRST, clearline);
-      }
-      digitalWrite(latchPin, HIGH);
+      clearRow(); //clear the row
     }
     else if (c == 'n') {  //clear registers for new data
       looping = false;
-      digitalWrite(latchPin, LOW);
-      for (int z = 0; z < 2; z++) { //change to reflect the number of shift registers as needed
-        shiftOut(dataPin, clockPin, MSBFIRST, clearline);
-      }
-      digitalWrite(latchPin, HIGH);
-      j=0; //reset arrays
-      i=0; //reset arrays
+      rendered = false;
+      clearRow();
+      j = 0; //reset arrays
+      i = 0; //reset arrays
     }
     else {
       readString += c; //makes the string readString
     }
   }
-
-  //shift data into registers, then enable outputs
-  if ((i == 16) || (looping)) { //make sure the value of i and y is the same as the
-    //number of rows set in the array intializer
-    for (int y = 0; y < 16; y++) {
-      digitalWrite(latchPin, LOW);
-      for (int x = 0; x < 2; x++) {
-        shiftOut(dataPin, clockPin, MSBFIRST, myArray[y][x]);
-      }
-      digitalWrite(latchPin, HIGH);
-      delay(onTime); //on time
-      digitalWrite(latchPin, LOW);
-      for (int z = 0; z < 2; z++) { //change to reflect the number of shift registers as needed
-        shiftOut(dataPin, clockPin, MSBFIRST, clearline);
-      }
-      digitalWrite(latchPin, HIGH);
-      delay (offTime); //off time
-    }
-      digitalWrite(latchPin, LOW); //after output sequence is sent, turn off all outputs
-      delay(onTime); //on time
-      for (int z = 0; z < 2; z++) { //change to reflect the number of shift registers as needed
-        shiftOut(dataPin, clockPin, MSBFIRST, clearline);
-      }
-      digitalWrite(latchPin, HIGH);
-      delay (offTime); //off time
-    i = 0;
-    data = 0;
+  if (i == 16) { //call the renderRow function if outputs when the array is filled,
+    renderRow();
+    rendered = true;
   }
+  if ((looping) && (rendered)) renderRow(); //looping is enabled only the rows have all been rendered once
 
 }
-
